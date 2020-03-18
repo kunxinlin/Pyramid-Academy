c60397d58b30795a434edb892ecf2adb7af1ef63
Kris <kunxinlin@gmail.com>
2020-03-18 00:38:28 -0400

    HumanVsGoblins

---
+++ HumansVsGoblins/src/Goblin.java
@@ -0,0 +1,5 @@
+public class Goblin {
+    static String goblin =  "\uD83D\uDC79";
+    static int[] position = {0, 0};
+
+}
---
+++ HumansVsGoblins/src/Human.java
@@ -0,0 +1,7 @@
+public class Human {
+    static String human =  "\uD83E\uDD3A";
+    static int[] position = {2, 4};
+
+
+
+}
